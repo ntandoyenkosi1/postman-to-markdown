888ab9e46560c6107f3fe208ea84c0672319b7f1
Ntandoyenkosi <nmendu@2u.com>
2023-10-17 15:27:25 +0200

    Added an HTML file and a basic server express

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
-const fs = require("fs")
+const express=require("express")
 
-const file = JSON.parse(fs.readFileSync("api.json", { encoding: "utf8" }))
-let finalFile = ""
-
-finalFile = `# ${file.info.name}
-${file.info.description}
-
-${file.item.map((x => {
-    return `[${x.name}](#${x.name.toLowerCase()})
-
-`
-})).join("")}
-
-${file.item.map((x => {
-    let y = x.item.map((z) => {
-        let name = z.name
-        let method = z.request?.method
-        let body = z.request?.body
-        let options={}
-        if (body?.options) {
-            options.mode=body.mode
-            options.language=body.options.raw.language
-            body = `\`\`\`${body?.options?.raw?.language}
-${body?.raw}
-\`\`\`\``
-        }
-
-        let path = z.request?.url?.path.map((x => {
-            return `/${x}`
-        })).join("")
-        return `##### **${method}** ${name}
-
-\`${path}\`
-
-${body ? `**Body** ${options.mode} (${options.language})
-${body}` : ""}
-`}).join("")
-    return `# ${x.name}
-${y}`
-})).join("")}`
-
-fs.writeFileSync("test.md", finalFile)
+const app=express()
+app.use(express.static("public"))
+app.get("*", (req,res)=>{
+    res.send("index.html")
+})
+console.log(process.env)
+app.listen(process.env.PORT||3001,()=>{
+    console.log(`Server is running at http://127.0.0.1:${process.env.PORT||3001}`)
+})
